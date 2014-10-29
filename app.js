267e7da23f652b3045ef09b530933c4719d8fe41
Suyash Mohan <suyash.mohan@shephertz.co.in>
2014-10-29 15:24:00 +0530

    http.get replaced with request

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 
 var redis = require('redis')
-var http = require('http')
+var request = require('request')
 var cheerio = require('cheerio')
 
 var client = redis.createClient(47346,"54.213.93.154", {auth_pass : "a2kw4qg9oo3tlauf1bgckq89mw4i7huc"})
 //var client = redis.createClient();
 
 var redisSaveLink = function(url, title, tags, callback){
-	/*client.incr('urlID', function(err, res){
-		client.hmset('url:'+res, {url : url, title:title ,tags :tags}, function(err, result){
-			callback(err);
-		});
-	});*/
 	client.rpush('urls',JSON.stringify({url : url, title:title ,tags :tags}), function(err, result){
 		callback(err);
 	});
@@ -19,24 +14,20 @@ var redisSaveLink = function(url, title, tags, callback){
 
 var app = {
 	saveLink : function(url, tags, callback){
-		//console.log("------------Saving URL : "+url);
-		http.get(url, function(res){
-			var result = "";
-			res.on('data', function(chunk){
-				result += chunk;
-			});
-			res.on('end', function(){
-				var $ = cheerio.load(result);
+		request(url, function(error, response, body){
+			if(response.statusCode  == 200){
+				var $ = cheerio.load(body);
 				var title = $('title').text();
 				redisSaveLink(encodeURI(url), title, tags, callback);
-			});
+			}
+			else{
+				callback(error);
+			}
 		});
 	},
 	
 	getAllLinks : function(callback){
-		//console.log("------------Getting All URLs");
 		client.lrange('urls', 0, -1, function(err, result){
-			//console.log("Error : " + err != null? 'No error' : err);
 			callback(result);
 		});
 	}
