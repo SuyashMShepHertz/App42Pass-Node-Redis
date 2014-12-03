d02fe332a70fe79c195a59b4051bf5514402aacd
Naveen Goswami <naveen.goswami@shephertz.co.in>
2014-12-03 21:44:17 +0530

    updated cred
    Signed-off-by: Naveen Goswami <naveen.goswami@shephertz.co.in>

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var redis = require('redis')
 var request = require('request')
 var cheerio = require('cheerio')
 
-var client = redis.createClient(52221,"54.213.93.154", {auth_pass : "anoqxyqqw0j028bzlc0opq1vmn8cfrts"})
+var client = redis.createClient(13856,"54.213.93.154", {auth_pass : "an8dmczjnkxsppv4j0ff30cuzx6u48bm"})
 //var client = redis.createClient();
 
 var redisSaveLink = function(url, title, tags, callback){
